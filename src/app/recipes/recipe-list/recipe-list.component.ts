bd5b848f2b8a6fab721563108ffc1ddab8c52b6b
saajan94 <saajan1994@aol.com>
2020-04-07 21:28:49 -0400

    Add components, navbar, complete app layout

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'This is a test recipe',
+      'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
